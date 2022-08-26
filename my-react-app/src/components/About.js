118f32d79b9324d126429d12a104001db7e86aa5
Shmuel Hoffman <snyh212@gmail.com>
2022-08-26 03:02:30 -0400

    moved files into main folder

--- my-react-app/src/components/About.js
+++ my-react-app/src/components/About.js
@@ -1,10 +1,10 @@
-import photo from "./images/camera.jpg"
+import photo from "./images/me.jpg"
 
 function About(){
     return(
         <div id="about">
             <h2>About Me</h2>
-            <img width="25%" src={photo} alt='self'></img>
+            <img width="15%" src={photo} alt='self'></img>
             <p>Hey there my names Shmuel Hoffman, I'm a WebDev student from Brooklyn, New York. I am currently enrolled in a UCB coding bootcamp gaining all of the skills necessary to be a master full-stack web developer!</p>
         </div>
     )
--- my-react-app/src/components/Portfolio.js
+++ my-react-app/src/components/Portfolio.js
@@ -11,11 +11,13 @@ function Portfolio(){
                 <p>Cocktail-Concocter</p>
                 <a target="_blank" rel="noreferrer" href="https://ericrothmuller.github.io/Cocktail-Concocter/">
                     <img width="25%" src={project1} alt='project1'/>
-                </a><br/>
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://github.com/ericrothmuller/Cocktail-Concocter">github.com/ericrothmuller/Cocktail-Concocter</a><br/>
                 <p>Simplicity-Art-Gallery</p>
                 <a target="_blank" rel="noreferrer" href="https://simplicity-art-gallery.herokuapp.com/">
                     <img width="25%" src={project2} alt='project2'/>
-                </a><br/>
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://github.com/dylankreisman/Art-gallery">github.com/dylankreisman/Art-gallery</a><br/>
                 <a target="_blank" rel="noreferrer" href="https://ericrothmuller.github.io/Cocktail-Concocter/">
                     <img width="25%" src={project1} alt='project'/>
                 </a><br/>
